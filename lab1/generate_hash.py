1f8a05d617fbd1a56ee297e9deb9629ac6623d4f
aa.mukhortova <aa.mukhortova@npcmr.ru>
2022-02-26 12:33:26 +0300

    Add tests, sorted generate

--- lab1/generate_hash.py
+++ lab1/generate_hash.py
@@ -1,11 +1,45 @@
 import hashlib
+import sys
+
+def main_gen(n):
+    new_file=open("input.txt",mode="w",encoding="utf-8")
 
 
-new_file=open("input.txt",mode="w",encoding="utf-8")
+    for num in range(0, n):
+        o = hashlib.sha1(bytes(str(num), encoding='utf-8')).hexdigest()
+        new_file.write(o+'\n')
+
+    new_file.close()
 
 
-for num in range(0, 1000):
-    o = hashlib.sha256(bytes(str(num), encoding='utf-8')).hexdigest()
-    new_file.write(o+'\n')
+def sorted_gen(n=1000, type='increase'):
+    new_file=open("input_sorted.txt",mode="w",encoding="utf-8")
 
-new_file.close()
+    if type == 'increase':
+        k = n
+    else:
+        k = 0
+    for num in range(0, n):
+        if k == 0:
+            num = hex(n - num)
+        else:
+            num = hex(n + num)
+        o = str(num).ljust(40, '0')
+        new_file.write(o+'\n')
+
+    new_file.close()
+    return
+
+
+def main():
+    n, type = 1000, 'increase'
+    if len(sys.argv) > 1:
+        n = int(sys.argv[1])
+    if len(sys.argv) > 2:
+        type = sys.argv[2]
+
+    main_gen(n)
+    sorted_gen(n , type)
+
+
+main()
---
+++ lab1/test.py
@@ -0,0 +1,63 @@
+import sys
+
+
+def increase(a, b):
+    return a <= b
+
+
+def decrease(a, b):
+    return a >= b
+
+
+def checker(list_num, dict_hash, func):
+    i, count = 1, 0
+    for obj in list_num[:len(list_num) - 1]:
+        if not func(obj, list_num[i]):
+            print ("Wrong place of hash " + dict_hash[list_num[i]], "line", i)
+            count+=1
+        i+=1
+    if count == 0:
+        print("No mistakes!")
+    else:
+        print(str(count) + " mistakes")
+
+
+def from_file():
+    
+    if (len(sys.argv) > 1):
+        file_name = sys.argv[1]
+    else:
+        file_name = "input.txt"
+    
+    file_hashes = open(file_name, "r")
+    n = file_hashes.readline()
+    list_numd = []
+    dict_hash = {}
+    
+    while n != '':
+        
+        if len(n) > 5:
+            l = int(n[:4], 16)
+            
+            if not l in dict_hash:
+                list_numd.append(l)
+                dict_hash[l] = n
+            else:
+                dict_hash[l] = dict_hash[l] + '\n' + n
+        
+        n = file_hashes.readline(). rstrip("\n")
+
+    file_hashes.close()
+    
+    return list_numd, dict_hash, file_name
+
+
+def main():
+    list_num, dict_hash, file_name = from_file()
+    if (file_name.find('decrease') != -1):
+        checker(list_num, dict_hash, decrease)
+    else:
+        checker(list_num, dict_hash, increase)
+    return
+
+main()

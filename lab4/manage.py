fab3b3f9784e199a5f410ee3d8d32d6727aa94ef
aa.mukhortova <aa.mukhortova@npcmr.ru>
2022-04-08 23:22:05 +0300

    Multiprocessing, more patterns in test

--- lab4/manage.py
+++ lab4/manage.py
@@ -1,19 +1,24 @@
 import hashlib
 from os import walk
 import find as fs
+from multiprocessing import Process
+import argparse
 
 
 def to_short_file(dict_adr):
     with open('short.bin', "wb") as file:
-        for adr in range (0, 2 ** 11):
+        for adr in range(0, 2 ** 11):
             offset = adr
             zippy = 0
+            nums = []
             if (adr in dict_adr):
                 for s in dict_adr[adr]:
-                    for n in range (0, 8):
+                    for n in range(0, 8):
                         num = 2 ** n
-                        if (s // num) % 2 != 0:
+
+                        if (zippy // num) % 2 == 0 and (s // num) % 2 != 0:
                             zippy += num
+
                 zippy = [zippy]
                 file.seek(offset)
                 file.write(bytes(zippy))
@@ -23,8 +28,10 @@ def to_short_file(dict_adr):
                 file.write(bytes(zippy))
 
 
-def patterns_from_file(hashes, dict_hashes, dict_adr, file_name, dict_patterns):
+def patterns_from_file(hashes, dict_hashes, dict_adr,
+                       file_name, dict_patterns):
     with open(file_name, "r") as file:
+
         for line in file:
             line = str(line).rstrip('\r\n')
             o = hashlib.sha1(bytes(str(line), encoding='utf-8')).hexdigest()
@@ -33,43 +40,40 @@ def patterns_from_file(hashes, dict_hashes, dict_adr, file_name, dict_patterns):
             adr = p[0:11]
             tail = p[11:35]
             zippy = 0
-            if not o in dict_patterns:
+
+            if o not in dict_patterns:
                 dict_patterns[o] = []
             dict_patterns[o].append(line)
-            if not adr in dict_hashes:
+
+            if adr not in dict_hashes:
                 dict_hashes[adr] = []
             dict_hashes[adr].append(o)
             b = bytes(int(adr, 2))
+
             for i in range(0, len(tail) - 1, 3):
                 num = int(tail[i:i+3], 2)
                 num = 2 ** num
+
                 if (zippy // num) % 2 == 0:
                     zippy += num
+
             if not int(adr, 2) in dict_adr:
                 dict_adr[int(adr, 2)] = []
+
             dict_adr[int(adr, 2)].append(zippy)
-    return 
+
+    return 0
 
 
-def main():
-    file_name = './tests/patterns1.txt'
-    hashes = []
-    dict_hashes = {}
-    dict_adr = {}
-    text = []
-    dict_patterns = {}
-    patterns_from_file(hashes, dict_hashes, dict_adr, file_name, dict_patterns)
-    to_short_file(dict_adr)
-    with open('./tests/text1.txt', "r", encoding='utf-8') as file:
-        
-        text = file.read()
-    with open('short.bin', "rb") as file:
-        print("I'm working. Wait, please.\n")
-        hashes, strs, adrs, zps, indexes = fs.find(0, len(text), len(dict_patterns[hashes[0]][0]), text, file)
-        
-        count = 0
+def proc(first, text_len, pattern_len, text,
+         file, dict_adr, dict_hashes, dict_patterns):
+    hashes, strs, adrs, zps, indexes = fs.find(first, text_len,
+                                               pattern_len, text, file)
 
-        for i in range(0, len(adrs)):
+    count = 0
+    print(len(adrs))
+    for i in range(0, len(adrs)):
+        if adrs[i] in dict_adr:
             for zp in dict_adr[adrs[i]]:
                 if zp == zps[i]:
                     adr = bin(int(adrs[i]))[2:13]
@@ -77,14 +81,62 @@ def main():
                         if hash == hashes[i]:
                             for string in dict_patterns[hash]:
                                 if string == strs[i]:
-                                    count+= 1
+                                    count += 1
                                     print('Found pattern:\n', string)
                                     print('Offset in str symbols:\n', indexes[i])
-        
-        print('Number of found pattens:', count)
 
-    print("\nThat's all. Thank you for waiting!\n")
+    print('Number of found pattrens (process', str(first) + '):', count)
+
+
+def parse_args(text_file, file_name):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--text', type=str,
+                        help='text file')
+    parser.add_argument('-p', '--pat', type=str,
+                        help='pattern file')
+    args = vars(parser.parse_args())
+
+    if(args['text']):
+        text_file[0] = args['text']
+
+    if(args['pat']):
+        file_name[0] = args['pat']
+
+
+def multiprocessing_n_working(text, dict_patterns, dict_adr,
+                              hashes, dict_hashes):
+    with open('short.bin', "rb") as file:
+        p = []
+
+        for j in range(0, 4):
+            p.append(0)
+            p[j] = Process(target=proc, args=(j, len(text),
+                           len(dict_patterns[hashes[0]][0]), text, file,
+                           dict_adr, dict_hashes, dict_patterns))
+
+        for j in range(0, 4):
+            p[j].start()
+
+        for j in range(0, 4):
+            p[j].join()
+
+
+def main():
+    file_name = './tests/patterns1.txt'
+    text_file = './tests/text1.txt'
+    hashes, dict_hashes, dict_adr, text, dict_patterns = [], {}, {}, [], {}
+
+    parse_args([text_file], [file_name])
+    patterns_from_file(hashes, dict_hashes, dict_adr, file_name, dict_patterns)
+    to_short_file(dict_adr)
+
+    with open(text_file, "r", encoding='utf-8') as file:
+
+        text = file.read()
+
+    multiprocessing_n_working(text, dict_patterns,
+                              dict_adr, hashes, dict_hashes)
 
 
 if __name__ == '__main__':
-    main()   
+    main()
